bd7dd6b1f0fe918457650ec76a7250122cdb12a7
nayetdet <jpmoreiracampos21@gmail.com>
2025-12-31 11:50:25 -0300

    chore: ajustada pipeline de execução

--- src/gatekeeper/config.py
+++ src/gatekeeper/config.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from typing import Any
+from typing import Any, Optional
 from hcaptcha_challenger import AgentConfig, FastShotModelType, SCoTModelType
 from pydantic import Field, field_validator
 from pydantic_core.core_schema import FieldValidationInfo
@@ -11,7 +11,7 @@ class Config(AgentConfig):
     EPIC_GAMES_PASSWORD: str = Field(default_factory=lambda: os.getenv("EPIC_GAMES_PASSWORD"))
     EPIC_GAMES_LOCALE: str = Field(default_factory=lambda: os.getenv("EPIC_GAMES_LOCALE"))
     EPIC_GAMES_COUNTRY: str = Field(default_factory=lambda: os.getenv("EPIC_GAMES_COUNTRY"))
-    EPIC_GAMES_CRONTAB: str = Field(default_factory=lambda: os.getenv("EPIC_GAMES_CRONTAB"))
+    EPIC_GAMES_CRONTAB: Optional[str] = Field(default_factory=lambda: os.getenv("EPIC_GAMES_CRONTAB", default=None))
 
     # Models
     MODEL: str = "gemini-2.5-flash"
